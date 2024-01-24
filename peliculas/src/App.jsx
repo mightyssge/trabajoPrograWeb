240659b933308c84c44a56a177479468a923d0fb
mightyssge <118016003+mightyssge@users.noreply.github.com>
2024-01-24 09:26:41 -0500

    a

--- peliculas/src/App.jsx
+++ peliculas/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/Header.jsx'
+import Header from './components/Header.jsx';
 import Sidebar from './components/Sidebar.jsx';
 import Content from './components/Content.jsx';
 import {Box , Stack} from "@mui/material";
