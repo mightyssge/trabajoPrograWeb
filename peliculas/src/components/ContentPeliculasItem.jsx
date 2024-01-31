24dbaf608f4fd67079ddc268fd2b7e4dee5056b0
Frossal <zluis1913@gmail.com>
2024-01-31 16:51:15 -0500

    Avance pantallas listo/ Luis

--- peliculas/src/components/ContentPeliculasItem.jsx
+++ peliculas/src/components/ContentPeliculasItem.jsx
@@ -1,69 +1,147 @@
 import React, { useState } from 'react';
-import { Container, Box, Button, TextField, Divider, Stack, Grid, Card, Paper, Avatar } from '@mui/material';
+import { Container, Box, Button, TextField, Divider, Stack, Grid, Card, Paper, Avatar, CardHeader } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Chip from '@mui/material/Chip';
 import Typography from '@mui/material/Typography';
 import { Link } from "react-router-dom"
+import { blue, red } from '@mui/material/colors';
+import '@fontsource/roboto'; 
+
+
+
+
 
 
 const ContentPeliculasItem = () => {
-
+ 
   return (
 
-    <Box flex={7} sx={{ p: 4 }} >
-      <Typography variant="h4" component="div" style={{ paddingTop: '16px', paddingBottom: '16px', borderBottom: '1px solid rgb(224, 224, 224)' }}>
+    <Box flex={2} sx={{ p: 4 }} >
+      <Typography variant="h4" component="div" sx={{ paddingTop: '16px', paddingBottom: '16px', borderBottom: '1px solid rgb(224, 224, 224)' }}>
         Películas
       </Typography>
 
-      <Box sx={{ mt: 4, padding: '0px 24px 0 24px', mr: 2, width: "95%", height: "1057px", mt: 5 }}>
-        <Container >
-          <header>
-            <Box >
-              <Typography variant="h4" sstyle={{ fontSize: "40px", fontFamily: "Roboto" }}  >Beekeeper Sentencia de Muerte</Typography>
+      <Box sx={{  padding: 4, mt: 5 }}>   
+            <Grid  item  md={4} sx={{ mb:1 }} >
+              <Typography variant="h4" sx={{ fontSize: "40px", fontFamily: "Roboto" }}  >Beekeeper Sentencia de Muerte</Typography>
 
-              <Grid style={{ display: "flex" }}>
-                <LocationOnIcon color="action" style={{ marginRight: "15px", marginBottom: "20px", marginTop: "10px" }} ></LocationOnIcon>
-                <Typography variant="subtitle2" color="#2196F3" fontWeight="600" style={{ marginBottom: "20px", marginTop: "10px" }}>
+              <Grid  sx={{ display: "flex" }}>
+                <LocationOnIcon color="action" sx={{ marginRight: "15px", marginBottom: "20px", marginTop: "10px" }} ></LocationOnIcon>
+                <Typography variant="subtitle2" color="#2196F3" fontWeight="600" sx={{ marginBottom: "20px", marginTop: "10px" }}>
                   1hrs 50min
                 </Typography>
-                <LocationOnIcon color='action' style={{ marginLeft: "15px", marginRight: "15px", marginBottom: "20px", marginTop: "10px" }} ></LocationOnIcon>
-                <Typography variant="subtitle2" color="#2196F3" fontWeight="600" style={{ marginBottom: "20px", marginTop: "10px", fontFamily: "Roboto" }}>
+                <LocationOnIcon color='action' sx={{ marginLeft: "15px", marginRight: "15px", marginBottom: "20px", marginTop: "10px" }} ></LocationOnIcon>
+                <Typography variant="subtitle2" color="#2196F3" fontWeight="600" sx={{ marginBottom: "20px", marginTop: "10px", fontFamily: "Roboto" }}>
                   Director
                 </Typography>
               </Grid>
-            </Box>
-          </header>
-        </Container>
+            </Grid>
 
 
-        <Grid display={"flex"} style={{ width: "100%", height: "340px" }}   >
-          <Card sx={{ marginRight: "6%" }} style={{ width: "700px", height: "340px" }}>
 
+
+
+
+      <Grid  display="flex" item>
+  <Card sx={{  }}>
+    <Grid item md={4} sx={{ height: "100%", width: "100%" }}>
+      <img
+        src="https://i.ytimg.com/vi/J2pWkhP3ou0/maxresdefault.jpg"
+        style={{ width: "100%", height: "100%" }}
+      />
+    </Grid>
+  </Card>
+
+  <Grid
+    sx={{
+      ml: { xs: 0, md: 5 },
+      height: "100%",
+      flexDirection: { xs: 'column', md: 'row' },
+      width: { xs: '100%', md: 'auto' },
+    }}
+  >
+    <Card sx={{ height: "100%", width: "100%" }}>
+      <Grid sx={{ mr: { xs: 0, md: 3 } }}>
+        <header typeof="title">
+          <Typography variant="h5" style={{ margin: "8%" }}>
+            Sinopsis
+          </Typography>
+        </header>
+
+        <Typography
+          variant="body1"
+          style={{
+            margin: "8%",
+            fontSize: "16px",
+            fontFamily: "Roboto",
+          }}
+        >
+          La brutal campaña de venganza de un hombre adquiere dimensiones nacionales cuando se descubre que es un antiguo agente de una poderosa organización clandestina conocida como "Los apicultores".
+        </Typography>
+
+        <Box sx={{ mt: '16px', display: 'flex', gap: '8px', margin: "5%" }}>
+          <Chip
+            label="Accion"
+            variant="filled"
+            color="default"
+            style={{ padding: "4px", borderRadius: "100px" }}
+          />
+          <Chip
+            label="+14"
+            variant="filled"
+            color="default"
+            style={{ padding: "4px", borderRadius: "100px" }}
+          />
+        </Box>
+      </Grid>
+    </Card>
+  </Grid>
+</Grid>
+
+ 
+
+         {/* <Grid  display={"flex"}  item  md xs  >
+   
+            <Card sx={{width:"160%"}} >
+             <Grid item md={4} sx={{ height:"100%",width:"100%"}} >
             <img
               src="https://i.ytimg.com/vi/J2pWkhP3ou0/maxresdefault.jpg"
-              style={{ width: "700px", height: "100%" }} />
+              style={{width:"100%", height:"100%"}}
+             />
+              
+              </Grid>
+            </Card>
+            
+            <Grid>
+              
 
-          </Card>
 
-          <Card style={{ width: "320px", height: "340px" }}>
+            <Grid sx={{ml:5, height:"100%"}}>
+            <Card sx={{ height:"100%", width:"79%"}} >
+              <Grid sx={{mr:3 }}>
 
-            <header >
-              <Typography variant='h5' style={{ margin: "10%" }} > Sinopsis</Typography>
+            <header typeof='title'>
+              <Typography variant='h5' style={{margin:"8%" }} > Sinopsis</Typography>
             </header>
 
-            <Typography variant='body1' style={{ margin: "8%", paddingRight: "0px", fontSize: "16px", fontFamily: "Roboto" }}>
+            <Typography variant='body1' style={{ margin: "8%",  fontSize: "16px", fontFamily: "Roboto" }}>
               La brutal campaña de venganza de un hombre adquiere dimensiones nacionales cuando se descubre que es un antiguo agente de una poderosa organización clandestina conocida como "Los apicultores".
             </Typography>
-            <Box sx={{ mt: '16px', display: 'flex', gap: '8px', margin: "5%" }} spacing={8}>
+            <Box sx={{ mt: '16px', display: 'flex', gap: '8px', margin: "5%" }} >
               <Chip label="Accion" variant="filled" color="default" style={{ padding: "4px", borderRadius: "100px" }} />
               <Chip label="+14" variant="filled" color="default" style={{ padding: "4px", borderRadius: "100px" }} />
             </Box>
 
+            </Grid>
+
           </Card>
 
         </Grid>
+        </Grid>
+        </Grid>*/}
+        
 
-        <Grid style={{ paddingTop: "2%", width: "100%", height: "58px", paddingBottom: "3%" }}>
+        <Grid style={{ paddingTop: "2%", width: "100%" }}item  md={4} >
           <Typography variant="h2" style={{ fontSize: "45px", fontFamily: "Roboto" }}>
             Salas disponibles
           </Typography>
@@ -71,11 +149,11 @@ const ContentPeliculasItem = () => {
 
 
 
-        <Box sx={{ mt: 10, width: "30%", height: "1057px" }} >
-          <Grid style={{ marginBottom: "18%" }}>
-            <Grid>
-              <Container style={{ width: "520px", height: "96px" }}>
-                <Container style={{ display: "flex", marginBottom: "16px" }}>
+        <Box sx={{ mt: 10, width: "55%", height: "100%" }} >
+          <Grid  style={{ }}item  md={4}>
+            <Grid item  md={4}> 
+              <Container style={{ width: "100%", height: "100%" }}>
+                <Container style={{ display: "flex", marginBottom: "4%" }}>
                   <Avatar variant='square' >
                     <Typography >
                       S1
@@ -92,7 +170,7 @@ const ContentPeliculasItem = () => {
                 </Typography>
               </Container>
 
-              <Grid sx={{ display: "flex" }}>
+              <Grid sx={{ display: "flex", ml:4, mb:5 }}>
                 <Box
 
                   sx={{
@@ -141,8 +219,8 @@ const ContentPeliculasItem = () => {
           </Grid>
 
           <Grid style={{ marginBottom: "18%" }}>
-            <Grid>
-              <Container style={{ width: "520px", height: "96px" }}>
+            <Grid item  md={4}>
+              <Container style={{width: "100%", height: "100%"  }}>
                 <Container style={{ display: "flex", marginBottom: "16px" }}>
                   <Avatar variant='square' >
                     <Typography >
@@ -160,7 +238,7 @@ const ContentPeliculasItem = () => {
                 </Typography>
               </Container>
 
-              <Grid sx={{ display: "flex" }}>
+              <Grid sx={{ display: "flex",ml:4, mb:5 }}>
                 <Box
 
                   sx={{
@@ -210,7 +288,7 @@ const ContentPeliculasItem = () => {
 
           <Grid style={{ marginBottom: "18%" }}>
             <Grid>
-              <Container style={{ width: "520px", height: "96px" }}>
+              <Container style={{ width: "100%", height: "100%"  }}>
                 <Container style={{ display: "flex", marginBottom: "16px" }}>
                   <Avatar variant='square' >
                     <Typography >
@@ -228,7 +306,7 @@ const ContentPeliculasItem = () => {
                 </Typography>
               </Container>
 
-              <Grid sx={{ display: "flex" }}>
+              <Grid sx={{ display: "flex",ml:4, mb:5 }}>
                 <Box
 
                   sx={{
